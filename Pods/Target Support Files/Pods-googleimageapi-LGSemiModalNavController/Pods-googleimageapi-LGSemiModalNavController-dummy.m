78254da5b2fecd58e597011c8b642f6462da5196
Luke Geiger <lukejamesgeiger@gmail.com>
2015-07-10 16:52:30 -0400

    Untracked

---
+++ Pods/Target Support Files/Pods-googleimageapi-LGSemiModalNavController/Pods-googleimageapi-LGSemiModalNavController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleimageapi_LGSemiModalNavController : NSObject
+@end
+@implementation PodsDummy_Pods_googleimageapi_LGSemiModalNavController
+@end
